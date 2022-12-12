4b352bca5c655bfc21cf35aaf3a660db139e0c55
nokhnaton <sage_of_forest@icloud.com>
2022-12-12 13:08:57 +0900

    とりあえずランダムで背景の色の濃さが変化するようにした。

---
+++ src/components/calender/DayBlock.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { format } from 'date-fns'
+defineProps<{
+  date: Date
+  parsentage: number
+}>()
+const getDateString = (date: Date) => {
+  if (date.getDate() === 1) {
+    return format(date, 'MM/dd')
+  }
+  return format(date, 'dd')
+}
+</script>
+<template>
+  <div :class="$style.body">
+    <span>{{ getDateString(date) }}</span>
+  </div>
+</template>
+<style module>
+.body {
+  background-color: rgba(255, 100, 100, v-bind(parsentage));
+  height: 100%;
+  margin: -0.1rem;
+  border: 0.1rem solid red;
+}
+</style>
